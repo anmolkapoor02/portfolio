f785a7d1237d5b3b6a828f8b01698ab810486187
anmolkapoor02 <anmolkapoor210@gmail.com>
2026-07-30 17:21:37 +0530

    vercel link change

--- src/constants/index.js
+++ src/constants/index.js
@@ -139,7 +139,7 @@ const socials = [
     text: "Platform",
     icon: "/icons/atom.svg",
     bg: "#4bcb63",
-    link: "https://jsmastery.com/",
+    link: "https://vercel.com/anmol-kapoor",
   },
   {
     id: 3,
