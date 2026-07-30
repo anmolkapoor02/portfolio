90672852283a1db5e8312e67aa546f2a3119182f
anmolkapoor02 <anmolkapoor210@gmail.com>
2026-07-30 17:00:42 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+
+import { Navbar, Welcome, Dock, Home } from "#components";
+import { Finder ,Resume, Safari, Terminal, Text, Image, Contact } from "#windows";
+import Photos from "#windows/Photos";
+
+gsap.registerPlugin(Draggable);
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Welcome />
+      <Dock />
+      <Terminal />
+      <Safari />
+      <Resume />
+      <Finder />
+      <Text />
+      <Image />
+      <Contact />
+      <Home />
+      <Photos />
+    </main>
+  );
+};
+
+export default App; 
---
+++ src/windows/index.js
@@ -0,0 +1,8 @@
+export { default as Terminal } from "./Terminal.jsx";
+export { default as Safari } from "./Safari.jsx";   
+export { default as Resume } from "./Resume.jsx";
+export { default as Finder } from "./Finder.jsx";
+export { default as Text } from "./Text.jsx";
+export { default as Image } from "./Image.jsx";
+export { default as Contact } from "./Contact.jsx";
+export { default as Photos } from "./Photos.jsx";
